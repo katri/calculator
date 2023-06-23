b5585eb4217e240fafed47a493366e24e70b9f65
Katri Pokats <katri.pokats@gmail.com>
2023-06-24 00:35:10 +0300

    Remove Lombok for thread safety

--- calculator/src/main/java/apps/calculator/NumberObject.java
+++ calculator/src/main/java/apps/calculator/NumberObject.java
@@ -1,10 +1,7 @@
 package apps.calculator;
 
-import lombok.Data;
-
 import java.io.Serializable;
 
-@Data
 public class NumberObject implements Serializable {
     int number1;
     int number2;
@@ -14,5 +11,17 @@ public class NumberObject implements Serializable {
         this.number1 = number1;
         this.number2 = number2;
         this.sum = number1 + number2;
+    }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public int getSum() {
+        return sum;
     }
 }
