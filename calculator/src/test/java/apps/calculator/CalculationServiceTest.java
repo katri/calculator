70f23dbf38e43df29665e1178ee59648b897b47c
Katri Pokats <katri.pokats@gmail.com>
2023-06-24 00:27:07 +0300

    Add tests to Calculation Service for search function

--- calculator/src/test/java/apps/calculator/CalculationServiceTest.java
+++ calculator/src/test/java/apps/calculator/CalculationServiceTest.java
@@ -2,6 +2,8 @@ package apps.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -37,5 +39,63 @@ class CalculationServiceTest {
         NumberObject expectedNumberObject = calculationService.createNumberObject(number1, number2);
 
         assertNull(expectedNumberObject);
+    }
+
+    @Test
+    void searchNumber_numberExistsAscending() {
+        List<NumberObject> numberObjects = List.of(
+                new NumberObject(1, 1),
+                new NumberObject(2, 44),
+                new NumberObject(7, 44),
+                new NumberObject(3, 2));
+
+        CalculationService calculationService = new CalculationService();
+        List<NumberObject> expectedNumberObjects = calculationService.searchNumber(2, true, numberObjects);
+        NumberObject element1 = expectedNumberObjects.get(0);
+        NumberObject element2 = expectedNumberObjects.get(1);
+        NumberObject element3 = expectedNumberObjects.get(2);
+
+        assertEquals(3, expectedNumberObjects.size());
+
+        assertEquals(1, element1.number1);
+        assertEquals(1, element1.number2);
+        assertEquals(2, element1.sum);
+
+        assertEquals(3, element2.number1);
+        assertEquals(2, element2.number2);
+        assertEquals(5, element2.sum);
+
+        assertEquals(2, element3.number1);
+        assertEquals(44, element3.number2);
+        assertEquals(46, element3.sum);
+    }
+
+    @Test
+    void searchNumber_numberExistsDescending() {
+        List<NumberObject> numberObjects = List.of(
+                new NumberObject(1, 1),
+                new NumberObject(2, 44),
+                new NumberObject(7, 44),
+                new NumberObject(3, 2));
+
+        CalculationService calculationService = new CalculationService();
+        List<NumberObject> expectedNumberObjects = calculationService.searchNumber(2, false, numberObjects);
+        NumberObject element1 = expectedNumberObjects.get(0);
+        NumberObject element2 = expectedNumberObjects.get(1);
+        NumberObject element3 = expectedNumberObjects.get(2);
+
+        assertEquals(3, expectedNumberObjects.size());
+
+        assertEquals(2, element1.number1);
+        assertEquals(44, element1.number2);
+        assertEquals(46, element1.sum);
+
+        assertEquals(3, element2.number1);
+        assertEquals(2, element2.number2);
+        assertEquals(5, element2.sum);
+
+        assertEquals(1, element3.number1);
+        assertEquals(1, element3.number2);
+        assertEquals(2, element3.sum);
     }
 }
